d71ccaa44583c6e20e60a52b8a512adf90e70966
ArcticZeroo <ArcticZeroo@users.noreply.github.com>
2015-02-07 14:50:00 -0500

    Update content_script.js

--- Extension/content_script.js
+++ Extension/content_script.js
@@ -35,7 +35,10 @@ function handleText(textNode)
 	v = v.replace(/\bsamia\b/g, "queen");
 	v = v.replace(/\batheist\b/g, "idiot");
 	v = v.replace(/\bAtheist\b/g, "Idiot");	
-  v = v.replace(/\bFedora\b/g, "Cock Sleeve");
-  v = v.replace(/\bfedora\b/g, "cock sleeve");	
+	v = v.replace(/\bFedora\b/g, "Cock Sleeve");
+  	v = v.replace(/\bfedora\b/g, "cock sleeve");
+    	v = v.replace(/\bfedora\b/g, "cock sleeve");
+    	v = v.replace(/\bfedoras\b/g, "cock sleeves");
+    	v = v.replace(/\bfedoras\b/g, "Cock Sleeves");
 	textNode.nodeValue = v;
 }
